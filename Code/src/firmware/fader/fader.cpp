2bf88f20e25a01d749614e2bec7026d6d58c1088
tommie1236 <90513578+Tommie1236@users.noreply.github.com>
2024-08-03 23:18:54 +0200

    first version of "following fader"
    works but is only sort of smooth at lower voltages and thus lower motor speeds. needs pid later probably. now it is just full speed up or down.

--- Code/src/firmware/fader/fader.cpp
+++ Code/src/firmware/fader/fader.cpp
@@ -4,32 +4,105 @@
 #include "hardware/gpio.h"
 #include "hardware/spi.h"
 
+// Define SPI pins
+#define SPI_PORT spi0
+#define PIN_SCK  18
+#define PIN_MOSI 19
+#define PIN_MISO 16
+#define PIN_CS   17
+
+// Function to initialize the SPI
+void spi_init_mcp3008() {
+    spi_init(SPI_PORT, 1000 * 1000);  // 5 MHz
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_init(PIN_CS);  
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1);
+}
+
+// Function to read a value from the MCP3008
+uint16_t read_adc(uint8_t channel) {
+    uint8_t buf[3];
+    uint16_t result;
+
+    buf[0] = 0x01;  // Start bit
+    buf[1] = (0x08 | channel) << 4;  // Channel configuration
+    buf[2] = 0x00;  // Don't care byte
+
+    gpio_put(PIN_CS, 0);
+    spi_write_read_blocking(SPI_PORT, buf, buf, 3);
+    gpio_put(PIN_CS, 1);
+
+    result = ((buf[1] & 0x03) << 8) | buf[2];  // Combine the result bytes
+    return result;
+};
  
-MFader::MFader(int index) : index(index), touched(0), position(0) {}
+
+
+MFader::MFader(int index) : index(index), touched(0), position(0) {
+    //temp testing code
+    if (this->index == 0) {
+        gpio_init(10);
+        gpio_init(11);
+        gpio_set_dir(10, GPIO_OUT);
+        gpio_set_dir(11, GPIO_OUT);
+    } else if (this->index == 1) {
+        gpio_init(12);
+        gpio_init(13);
+        gpio_set_dir(12, GPIO_OUT);
+        gpio_set_dir(13, GPIO_OUT);
+    }
+}
 
 // TODO: change to proper adc read later
 int MFader::updatePosition() {
-    uint16_t value = 0/*read_adc(this->index)*/;
+    uint16_t value = read_adc(this->index);
     return value * 0.5;
 };
 
 // TODO:
 void MFader::moveUp() {
     if (!this->touched) {
-        // pin x high, pin y low
+        // pin A high, pin B low
+        // temp testing code below
+        if (this->index == 0) {
+            gpio_put(10, 1);
+            gpio_put(11, 0);
+        } else if (this->index == 1) {
+            gpio_put(12, 1);
+            gpio_put(13, 0);
+        }
     };
 };
 
 // TODO:
 void MFader::moveDown() {
     if (!this->touched) {
-        // pin x low, pin y high
+        // pin A low, pin B high
+        // temp testing code below
+        if (this->index == 0) {
+            gpio_put(10, 0);
+            gpio_put(11, 1);
+        } else if (this->index == 1) {
+            gpio_put(12, 0);
+            gpio_put(13, 1);
+        }
     };
 };
 
 // TODO:
 void MFader::moveStop() {
-    // pin x low, pin y low
+    // pin A low, pin B low
+    // temp test code below
+    if (this->index == 0) {
+        gpio_put(10, 0);
+        gpio_put(11, 0);
+    } else if (this->index == 1) {
+        gpio_put(12, 0);
+        gpio_put(13, 0);
+    }
 };
 
 int MFader::getPosition() {
@@ -67,37 +140,3 @@ bool MFader::setTouched(bool value) {
 
 
 
-// Define SPI pins
-#define SPI_PORT spi0
-#define PIN_SCK  18
-#define PIN_MOSI 19
-#define PIN_MISO 16
-#define PIN_CS   17
-
-// Function to initialize the SPI
-void spi_init_mcp3008() {
-    spi_init(SPI_PORT, 1000 * 1000);  // 5 MHz
-    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_init(PIN_CS);  
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-}
-
-// Function to read a value from the MCP3008
-uint16_t read_adc(uint8_t channel) {
-    uint8_t buf[3];
-    uint16_t result;
-
-    buf[0] = 0x01;  // Start bit
-    buf[1] = (0x08 | channel) << 4;  // Channel configuration
-    buf[2] = 0x00;  // Don't care byte
-
-    gpio_put(PIN_CS, 0);
-    spi_write_read_blocking(SPI_PORT, buf, buf, 3);
-    gpio_put(PIN_CS, 1);
-
-    result = ((buf[1] & 0x03) << 8) | buf[2];  // Combine the result bytes
-    return result;
-};
--- Code/src/firmware/main.cpp
+++ Code/src/firmware/main.cpp
@@ -7,11 +7,23 @@
 #include "fader/fader.h"
 #include "button/button.h"
 
-
-//#define LS30 // uncomment when using the ls30 debounc ic for the encoders
-
+#include "hardware/spi.h"
 
 
+//#define LS30 // uncomment when using the ls30 debounc ic for the encoders (not yet inplemented)
+
+
+#define SPI_PORT spi0
+#define PIN_SCK  18
+#define PIN_MOSI 19
+#define PIN_MISO 16
+#define PIN_CS   17
+
+
+#define FADER_1_A 10
+#define FADER_1_B 11
+#define FADER_2_A 12
+#define FADER_2_B 13
 
 
 
@@ -23,17 +35,18 @@ int main () {
     spi_init_mcp3008();
     stdio_init_all();
 
-    MFader fader0(0);
-    MFader fader1(1);
+    MFader fader_1(0);
+    MFader fader_2(1);
 
     gpio_put(LED_PIN, 1);
     printf("online\n");
 
-    while (0) {
-        int value0 = fader0.getPosition();
-        int value1 = fader1.getPosition();
-        printf("%d %d \n", value0, value1);
+    while (1) {
+        int value1 = fader_1.getPosition();
+        int value2 = fader_2.getPosition();
+        printf("%d %d \n", value1, value2);
         sleep_ms(100);
+        fader_1.moveToPosition(value2);
     };
     return 0;
 
