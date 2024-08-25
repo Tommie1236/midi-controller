44275af6edf925a2e269021344a8941e0c77a290
tommie1236 <90513578+Tommie1236@users.noreply.github.com>
2024-08-25 19:41:12 +0200

    split build.sh into 2 files
    hardcoded the files for now as i couldn't get a fancier script to work. (see last commit)

---
+++ build-fader-wing.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e    # Exit on error
+
+git submodule update --init
+cd Code/lib/pico-sdk/
+git submodule update 
+export PICO_SDK_PATH=$PWD
+cd ../..
+mkdir -p build
+cp ./lib/pico-sdk/external/pico_sdk_import.cmake .
+cd build
+cmake ..
+make -j $(($(nproc) * 2))
+
+while getopts 'l' flag; do
+  case "${flag}" in (l) 
+      echo "Loading firmware <$(pwd)/src/fader-wing/fader-wing.bin> to Pico"
+      sudo picotool load $(pwd)/src/fader-wing/fader-wing.bin
+      ;;
+  esac
+done
